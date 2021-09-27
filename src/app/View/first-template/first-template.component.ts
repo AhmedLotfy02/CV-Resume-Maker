f71858336e03f98e382f05598e88a5f9aa89ff0b
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-09-28 01:30:10 +0200

    Template 1 CV is added

--- src/app/View/first-template/first-template.component.ts
+++ src/app/View/first-template/first-template.component.ts
@@ -43,6 +43,78 @@ export class FirstTemplateComponent implements OnInit {
       console.log('Thing was not saved to the database.');
     }
   }
+  test2() {
+    if (confirm('Are you want to add another field?')) {
+      // Save it!
+      var cloned = document.getElementById('input2')!.cloneNode(true);
+      cloned.nodeValue = ' '; // cloned will have no value
+      document.getElementById('div2')!.appendChild(cloned);
+      console.log('Thing was saved to the database.');
+    } else {
+      // Do nothing!
+      console.log('Thing was not saved to the database.');
+    }
+  }
 
+  test3() {
+    if (confirm('Are you want to add another field?')) {
+      // Save it!
+      var cloned = document.getElementById('input3')!.cloneNode(true);
+      cloned.nodeValue = ' '; // cloned will have no value
+      document.getElementById('div3')!.appendChild(cloned);
+      console.log('Thing was saved to the database.');
+    } else {
+      // Do nothing!
+      console.log('Thing was not saved to the database.');
+    }
+  }
+  test4() {
+    if (confirm('Are you want to add another field?')) {
+      // Save it!
+      var cloned = document.getElementById('input4')!.cloneNode(true);
+      cloned.nodeValue = ' '; // cloned will have no value
+      document.getElementById('div4')!.appendChild(cloned);
+      console.log('Thing was saved to the database.');
+    } else {
+      // Do nothing!
+      console.log('Thing was not saved to the database.');
+    }
+  }
+  test5() {
+    if (confirm('Are you want to add another field?')) {
+      // Save it!
+      var cloned = document.getElementById('input5')!.cloneNode(true);
+      cloned.nodeValue = ' '; // cloned will have no value
+      document.getElementById('div5')!.appendChild(cloned);
+      console.log('Thing was saved to the database.');
+    } else {
+      // Do nothing!
+      console.log('Thing was not saved to the database.');
+    }
+  }
+  test6() {
+    if (confirm('Are you want to add another field?')) {
+      // Save it!
+      var cloned = document.getElementById('input6')!.cloneNode(true);
+      cloned.nodeValue = ' '; // cloned will have no value
+      document.getElementById('div6')!.appendChild(cloned);
+      console.log('Thing was saved to the database.');
+    } else {
+      // Do nothing!
+      console.log('Thing was not saved to the database.');
+    }
+  }
+  test7() {
+    if (confirm('Are you want to add another field?')) {
+      // Save it!
+      var cloned = document.getElementById('input7')!.cloneNode(true);
+      cloned.nodeValue = ' '; // cloned will have no value
+      document.getElementById('div7')!.appendChild(cloned);
+      console.log('Thing was saved to the database.');
+    } else {
+      // Do nothing!
+      console.log('Thing was not saved to the database.');
+    }
+  }
   ngOnInit(): void {}
 }
